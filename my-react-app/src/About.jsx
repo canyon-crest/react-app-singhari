35c7a0cadb87e435361da39eb43e28edf536e5e6
Ariana Singh <leafwing9@gmail.com>
2026-03-19 19:30:29 -0700

    errors for no reason, no idea why; added example/db usage in

--- my-react-app/src/About.jsx
+++ my-react-app/src/About.jsx
@@ -1,4 +1,5 @@
 import "./App.css"
+import Example from "./Example"
 import GoogleLogin from "./GoogleLogin"
 
 function About() {
@@ -6,6 +7,7 @@ function About() {
     return (
         <div>
             <GoogleLogin />
+            <Example />
             <p>this is where I talk about me</p>
             <p>I like dinosaurs so I made it about dinosaurs.</p>
         </div>
